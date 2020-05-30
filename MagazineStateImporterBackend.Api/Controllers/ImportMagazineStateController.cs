bfda5f4fffca7f04c5dc2e75207c8df8ae1a7714
Ewelina Olejnik <ewelina.olejnik@ts.fujitsu.com>
2020-05-30 13:55:08 +0200

    Remove unnecessary code; add ImportMagazineStateController

---
+++ MagazineStateImporterBackend.Api/Controllers/ImportMagazineStateController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagazineStateImporterBackend.Api.Controllers
+{
+    [Route("api/import-magazine-state")]
+    [ApiController]
+    public class ImportMagazineStateController : ControllerBase
+    {
+        
+    }
+}
